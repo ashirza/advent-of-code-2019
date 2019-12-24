f9f490b3e32f7244adaecf76415c88980ca65dc8
ashir zafar <ashirza@gmail.com>
2019-12-24 14:08:06 +0000

    finished first challenge

---
+++ one.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class One {
+    public static void main(String[] args) {
+        File file = new File("files/one-numbers.txt");
+        Scanner sc = null;
+        double totalFuelRequired = 0;
+
+        try{
+            sc = new Scanner(file);
+            while(sc.hasNextLine()) {
+                Double module = Double.valueOf(sc.nextLine());
+                double fuelRequired = Math.floor((module / 3)) - 2;
+                totalFuelRequired += fuelRequired;
+            }
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } finally{
+            sc.close();
+        }
+        System.out.println(totalFuelRequired);
+    }
+}
